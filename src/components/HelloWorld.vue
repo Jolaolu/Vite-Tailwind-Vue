5a07c196e23c279cee0485576664bbe4c657ef2a
Anjolaoluwa <adebayoanjola@gmail.com>
2020-06-18 17:52:09 +0100

    added sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
+  <input type="text" name="" class="" id="">
+  <button class=" text-white flex justify-center px-4 py-2 bg-blue-500" @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
@@ -17,3 +18,11 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+  input{
+    background: blue;
+    &:focus{
+      outline-color: red;
+    }
+  }
+</style>
