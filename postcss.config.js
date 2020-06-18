bcc7541bdbc13a773b9431b32809b0b3df6e23a1
Anjolaoluwa <adebayoanjola@gmail.com>
2020-06-18 17:39:45 +0100

    added tailwind

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const tailwind = require( 'tailwindcss' )
+const autoprefixer = require( 'autoprefixer' )
+
+module.exports = {
+    plugins: [
+        // ...
+        tailwind,
+        autoprefixer
+        // ...
+    ]
+}
